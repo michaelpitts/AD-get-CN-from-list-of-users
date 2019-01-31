3e86fa3f29bbd99467df9430ac098630f618d512
Michael Pitts <30473498+michaelpitts@users.noreply.github.com>
2019-01-31 16:09:53 -0500

    Update usersearch.ps1

--- usersearch.ps1
+++ usersearch.ps1
@@ -5,4 +5,4 @@ $users = (Import-CSV "M:\userlist-names.csv") | select -exp Name
 
 
 foreach ($user in $users) {
-    Get-ADUser -Filter {Name -eq $user } -SearchBase "DC=MED,DC=HARVARD,DC=EDU" -Properties CanonicalName | Out-File -filepath M:\outfile.txt -Append}
+    Get-ADUser -Filter {Name -eq $user } -SearchBase "DC=DOMAIN,DC=COM" -Properties CanonicalName | Out-File -filepath M:\outfile.txt -Append}
